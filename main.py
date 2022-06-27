0ac77a3f4b7ed1e72eab1e92d3523178773d6256
RutananaArnold <arnoldrutanana@gmail.com>
2022-06-27 19:26:55 +0300

    sending logic fixed

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ async def topup(sendMoney: SendMoney, user_id: int):
         if client.id == user_id:
             if sendMoney.balance is not None:
                 client.balance = client.balance + sendMoney.balance
-            return 
+            return client.balance
     raise HTTPException(
         status_code=404,
         detail=f"user with id: {user_id} does not exist"
@@ -50,6 +50,29 @@ async def withdraw(sendMoney: SendMoney, user_id: int):
         detail=f"user with id: {user_id} does not exist"
     )
 
-@app.post('/api/user/send/{sender_id}/{receiveruser_id}')
+@app.put('/api/user/send/{sender_id}/{receiveruser_id}')
 async def sendAmount(sendMoney: SendMoney, sender_id: int, receiveruser_id: int):
-    pass
+    # assign sender and receiver to objects
+    sender = {}
+    receiver = {}
+    
+    for user in db: 
+        print(user)
+        # check if sender exists
+        if sender_id == user.id:
+            sender = user
+        # check if receiver exists
+        if receiveruser_id == user.id:
+            receiver = user
+        
+    # check if sender has sufficient balance
+    if sender.balance >= sendMoney.balance:
+        sender.balance -= sendMoney.balance # reduce sender balance
+        receiver.balance += sendMoney.balance # increase receiver balance
+        return "Funds sent"
+    else:
+        return "Insuffient funds"  
+    
+    # reduce sender balance by amount to be sent
+    # increase receiver balance by amount
+       
